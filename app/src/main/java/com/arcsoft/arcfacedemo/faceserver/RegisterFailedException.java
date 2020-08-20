bc86e0e57edf9cbfea2788dcbd5ca1c659e8e1e9
zehao <zehao@fangyingmobile.com>
2020-08-20 15:25:15 +0800

    Revert "first commit"
    This reverts commit 8cd403931f1cfe840276a45cdfcfa6c75e329412.

--- app/src/main/java/com/arcsoft/arcfacedemo/faceserver/RegisterFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.arcsoft.arcfacedemo.faceserver;
-
-public class RegisterFailedException extends Exception {
-    public RegisterFailedException(String message) {
-        super(message);
-    }
-}
--- app/src/main/java/com/arcsoft/arcfacedemo/util/face/constants/RequestLivenessStatus.java
+++
@@ -1,5 +0,0 @@
-package com.arcsoft.arcfacedemo.util.face.constants;
-
-public class RequestLivenessStatus {
-    public static final int ANALYZING = 10;
-}
