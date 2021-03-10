3df20ca4334791950b84b5a013f54861dd780dc7
liguangchao <guangchao@gstdev.com>
2021-03-10 19:35:42 +0800

    change mac

--- app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
+++ app/src/main/java/com/arcsoft/arcfacedemo/common/Constants.java
@@ -10,10 +10,14 @@ public class Constants {
     /**
      * 方式一： 填好APP_ID等参数，进入激活界面激活
      */
-    public static final String APP_ID = "Bd9n7mCgG5b7PPmvWjh1h7Gdb9HBhFiQqpRmFH6ZqfU2";
-    public static final String SDK_KEY = "9LamVon7KCnJKtHhPxt5kwsWn2Gdhq4KDWtqn6jJxzVy";
-    public static final String ACTIVE_KEY = "85T1-113L-N11F-MGYX";//新
-//    public static final String ACTIVE_KEY = "85T1-113L-N13U-RURM";//旧
+    public static final String APP_ID = "AbVJr8QtzruZvhTPHzFT4DNyoKQBzMebCYnRNtR7m8jQ";
+    public static final String SDK_KEY = "aY43ojgUvG5a2gKdpgdRWZXgoqiem1cRCyuC3ipNAcw";
+//    public static final String APP_ID = "Bd9n7mCgG5b7PPmvWjh1h7Gdb9HBhFiQqpRmFH6ZqfU2";
+//    public static final String SDK_KEY = "9LamVon7KCnJKtHhPxt5kwsWn2Gdhq4KDWtqn6jJxzVy";
+//    public static final String APP_ID = "AbVJr8QtzruZvhTPHzFT4DNrdv927yKYXEisXt9YTWi6";
+//    public static final String SDK_KEY = "48eLJewBnNHgJ7t8NZB83rrf1PKxFqJHQFZvTJ11CZau";
+//    public static final String ACTIVE_KEY = "85T1-113L-N11F-MGYX";//新
+    public static final String ACTIVE_KEY = "85T1-1149-W112-6XK1";//旧
     /**
      * 方式二： 在激活界面读取本地配置文件进行激活
      *
